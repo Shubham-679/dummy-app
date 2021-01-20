7963fb6107a3a821f240eee827c5d4a7edfa4e56
Shubham-679 <kushwahshubham679.com>
2021-01-20 12:36:24 +0530

    heroku

--- task-manager-app/src/components/home.jsx
+++
@@ -1,9 +0,0 @@
-const Home = (props) => {
-    return ( 
-        <div className="mt-5">
-            <h1> Welcome User </h1>
-        </div>
-     );
-}
- 
-export default Home;
--- task-manager-app/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import taskReducer from "./taskReducer";
-import userReducer from './userReducer'
-
-export default combineReducers({
-    users : userReducer,
-    tasks : taskReducer
-})
